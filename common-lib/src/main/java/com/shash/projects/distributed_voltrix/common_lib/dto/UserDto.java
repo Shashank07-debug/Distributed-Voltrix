c2d4588f85452cbe3c37122412cd14f37899b3a8
shashank007 <shashankmn066@gmail.com>
2026-09-16 13:27:29 +0100

    added common-lib

---
+++ common-lib/src/main/java/com/shash/projects/distributed_voltrix/common_lib/dto/UserDto.java
@@ -0,0 +1,9 @@
+package com.shash.projects.distributed_voltrix.common_lib.dto;
+
+public record UserDto (
+    Long id,
+    String username,
+    String name
+){
+
+}
---
+++ common-lib/src/main/java/com/shash/projects/distributed_voltrix/common_lib/security/JwtUserPrinciple.java
@@ -0,0 +1,30 @@
+package com.shash.projects.distributed_voltrix.common_lib.security;
+
+import jakarta.annotation.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public record JwtUserPrinciple (
+        Long userId,
+        String username,
+        String password,
+        List<GrantedAuthority> authorities
+) implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername(){
+        return username;
+    }
+}
